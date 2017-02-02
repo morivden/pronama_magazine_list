03432ec22fea0bc1966bb2deccff12feb2ad18e2
morivden <fuziyamavolcano@yahoo.co.jp>
2017-02-02 10:57:27 +0900

    fail to add_origin_list

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -1,60 +1,60 @@
 class MagazinesController < ApplicationController
-    before_action :set_magazine, only: [:show, :edit, :update, :destroy]
+  before_action :set_magazine, only: [:show, :edit, :update, :destroy]
 
-    def title
+  def title
+  end
+
+  def index
+    # @magazines = Magazine.all
+    @search = Magazine.search(params[:q])
+    @magazines = @search.result(distinct: true)
+  end
+
+  def show
+  end
+
+  def new
+    @magazine = Magazine.new
+  end
+
+  def create
+    # 追加に成功 → 薄い本一覧に
+    # 追加に失敗 → やり直し
+    @magazine = Magazine.new(magazine_params)
+    @magazine.save
+    # バリデーション
+    if @magazine.save
+      redirect_to magazines_path
+    else
+      render 'new'
     end
+  end
 
-    def index
-        # @magazines = Magazine.all
-        @search = Magazine.search(params[:q])
-        @magazines = @search.result(distinct: true)
+  def edit
+  end
+
+  def update
+    # 更新に成功 → 薄い本情報一覧に
+    # 更新に失敗 → やり直し
+    if @magazine.update(magazine_params)
+      redirect_to magazine_path
+    else
+      render 'edit'
     end
+  end
 
-    def show
-    end
+  def destroy
+    @magazine.destroy
+    redirect_to magazines_path
+  end
 
-    def new
-        @magazine = Magazine.new
-    end
+  private
 
-    def create
-        # 追加に成功 → 薄い本一覧に
-        # 追加に失敗 → やり直し
-        @magazine = Magazine.new(magazine_params)
-        @magazine.save
-        # バリデーション
-        if @magazine.save
-            redirect_to magazines_path
-        else
-            render 'new'
-        end
-    end
+  def magazine_params
+    params[:magazine].permit(:title, :origin, :author, :circle, :qualify, :publication, :printed, :another_info)
+  end
 
-    def edit
-    end
-
-    def update
-        # 更新に成功 → 薄い本情報一覧に
-        # 更新に失敗 → やり直し
-        if @magazine.update(magazine_params)
-            redirect_to magazine_path
-        else
-            render 'edit'
-        end
-    end
-
-    def destroy
-        @magazine.destroy
-        redirect_to magazines_path
-    end
-
-    private
-
-    def magazine_params
-        params[:magazine].permit(:title, :origin, :author, :circle, :qualify, :publication, :printed, :another_info)
-    end
-
-    def set_magazine
-        @magazine = Magazine.find(params[:id])
-    end
+  def set_magazine
+    @magazine = Magazine.find(params[:id])
+  end
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,24 +1,24 @@
 class Magazine < ApplicationRecord
-    validates :title,
-    presence: {message: "タイトルを入力してください"}
+  validates :title,
+  presence: {message: "タイトルを入力してください"}
 
-    validates :origin,
-    presence: {message: "原作を入力してください"}
+  validates :origin,
+  presence: {message: "原作を入力してください"}
 
-    validates :author,
-    presence: {message: "作者を入力してください"}
+  validates :author,
+  presence: {message: "作者を入力してください"}
 
-    validates :circle,
-    presence: {message: "サークルを入力してください"}
+  validates :circle,
+  presence: {message: "サークルを入力してください"}
 
-    validates :qualify,
-    presence: {message: "対象年齢を入力してください"},
-    inclusion: {in: ["ALL", "R-18"], message: "R-18またはALLと入力してください"}
+  validates :qualify,
+  presence: {message: "対象年齢を入力してください"},
+  inclusion: {in: ["ALL", "R-18"], message: "R-18またはALLと入力してください"}
 
-    validates :publication,
-    presence: {message: "発行日を入力してください"},
-    format: {with: /\d\d\d\d.\d\d.\d\d/, message: "発行日は2000.01.01のような形式で入力してください"}
+  validates :publication,
+  presence: {message: "発行日を入力してください"},
+  format: {with: /\d\d\d\d.\d\d.\d\d/, message: "発行日は2000.01.01のような形式で入力してください"}
 
-    validates :printed,
-    presence: {message: "印刷所を入力してください"}
+  validates :printed,
+  presence: {message: "印刷所を入力してください"}
 end
